55579c2ddf319fb66bd6602fbd440947c181e5ed
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-19 12:44:37 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance<=400) {
+    return "This one is on me!";
+  } else if (distance >400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance >2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+  return "No can do.";
+  }
 }
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1000)); 
+console.log(scuberGreetingForFeet(2100));
+console.log(scuberGreetingForFeet(3000));
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === "NYC" ? "Ok, sounds good." : "No go.";
 }
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('LA'));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('average'));
